71145afa615289c0fd25b340b288581dc58a101b
Aedan Evangelista <103922745+aedanevangelista@users.noreply.github.com>
2025-03-30 12:38:45 +0800

    1

--- public/login.php
+++ public/login.php
@@ -52,7 +52,7 @@ $conn->close();
     <div class="login-container">
         <div class="form-content">
             <div class="formHeading">
-                <span class="h3">WELCOME BACK 👋</span> <br/>
+                <span class="h3">WELCOME BACK1 👋</span> <br/>
                 <span class="excerptOne">Enter your username and password to continue.</span>
             </div>
 
