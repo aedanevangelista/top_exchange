75cfa3a2b1599fb7644fbbd66c332caafdd17d1b
Aedan Evangelista <103922745+aedanevangelista@users.noreply.github.com>
2025-03-31 01:50:37 +0800

    1

--- backend/get_orders_by_date.php
+++ backend/get_orders_by_date.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include "db_connection.php";  // Update the path if needed
+include "/backend/db_connection.php";
 
 // Check if user is logged in
 if (!isset($_SESSION['user_id'])) {
