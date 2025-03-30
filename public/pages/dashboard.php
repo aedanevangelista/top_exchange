c1c73d7d7a39df6682e1a395e9ee3d3ea8e7b1d1
Aedan Evangelista <103922745+aedanevangelista@users.noreply.github.com>
2025-03-30 15:01:26 +0800

    1

--- public/pages/dashboard.php
+++ public/pages/dashboard.php
@@ -58,8 +58,8 @@ $clientOrders = getClientOrdersCount($conn, $selectedYear);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
-    <link rel="stylesheet" href="public_html/public/css/dashboard.php">
-    <link rel="stylesheet" href="public_html/public/css/sidebar.css"> 
+    <link rel="stylesheet" href="public/css/dashboard.css">
+    <link rel="stylesheet" href="public/css/sidebar.css"> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
