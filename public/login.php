6f4ebcb10a82dd2ad6449c8968ea2d5b855eb043
Aedan Evangelista <103922745+aedanevangelista@users.noreply.github.com>
2025-03-30 12:45:59 +0800

    1

--- public/login.php
+++ public/login.php
@@ -46,7 +46,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="123">
 </head>
 <body>
     <div class="login-container">
