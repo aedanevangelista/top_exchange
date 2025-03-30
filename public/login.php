b0a1f13929aa33c89df45867bd6425c3d914eaa5
Aedan Evangelista <103922745+aedanevangelista@users.noreply.github.com>
2025-03-30 14:33:18 +0800

    1

--- public/login.php
+++ public/login.php
@@ -51,7 +51,7 @@ $conn->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <!-- Updated CSS path to use absolute path from domain root -->
-    <link rel="stylesheet" href="https://topexchangefood.com/css/styles.css">
+    <link rel="stylesheet" href="/top_exchange/public/css/login.css">
 </head>
 <body>
     <div class="login-container">
@@ -83,6 +83,6 @@ $conn->close();
         </div>
     </div>
     <!-- Updated JavaScript path to use absolute path from domain root -->
-    <script src="https://topexchangefood.com/js/script.js"></script>
+    <script src="/top_exchange/public/js/login.js"></script>
 </body>
 </html>
