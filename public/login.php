77e7cb7fa4d6d49dc961a63f6dd0d952c8c5c03b
Aedan Evangelista <103922745+aedanevangelista@users.noreply.github.com>
2025-03-30 13:02:24 +0800

    1

--- public/login.php
+++ public/login.php
@@ -52,7 +52,7 @@ $conn->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <!-- Updated CSS path to use absolute path from domain root -->
-    <link rel="stylesheet" type="text/css" href="/css/login.css">
+    <link rel="stylesheet" type="text/css" href="css/login.css">
 </head>
 <body>
     <div class="login-container">
