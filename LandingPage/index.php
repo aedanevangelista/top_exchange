269490f28d62c2a52650b02481621a775d06ce5c
Aedan Evangelista <103922745+aedanevangelista@users.noreply.github.com>
2025-03-30 21:47:23 +0800

    1

--- LandingPage/index.php
+++ LandingPage/index.php
@@ -508,7 +508,7 @@ if (!isset($_SESSION['cart'])) {
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item active">
-                            <a class="nav-link" href="index.php">Home123123</a>
+                            <a class="nav-link" href="index.php">Home</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="/LandingPage/about.php">About</a>
