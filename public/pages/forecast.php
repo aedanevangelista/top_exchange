7391846782503782caa7f5c07e4f346a20506d32
Aedan Evangelista <103922745+aedanevangelista@users.noreply.github.com>
2025-03-31 01:27:33 +0800

    1

--- public/pages/forecast.php
+++ public/pages/forecast.php
@@ -71,6 +71,8 @@ $navigation = getMonthNavigation($month, $year);
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="/css/toast.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"></script>
     <style>
         .main-content {
             margin-left: 250px;
