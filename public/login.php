d9e4f71723357a439eb245f0f0e1059384321379
Aedan Evangelista <103922745+aedanevangelista@users.noreply.github.com>
2025-04-22 01:15:57 +0800

    11

--- public/login.php
+++ public/login.php
@@ -50,7 +50,7 @@ $conn->close();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Login</title>
+    <title>Admin Loginasdas</title>
 <style>
     .formHeading {
     margin-bottom: 40px;
