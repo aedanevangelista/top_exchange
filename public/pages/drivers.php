719defc7c41ce551bff6088b4c40d70bf63c9367
Aedan Evangelista <103922745+aedanevangelista@users.noreply.github.com>
2025-04-23 22:25:09 +0800

    1

--- public/pages/drivers.php
+++ public/pages/drivers.php
@@ -77,7 +77,7 @@ $result = $conn->query($sql);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Drivers Management</title>
+    <title>Drivers</title>
     <link rel="stylesheet" href="/css/dashboard.css">
     <link rel="stylesheet" href="/css/sidebar.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
