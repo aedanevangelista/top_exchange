326c64451a735e1f64005b5beabb6f6220400616
Aedan Evangelista <103922745+aedanevangelista@users.noreply.github.com>
2025-04-25 03:23:09 +0800

    1

--- public/pages/pending_orders.php
+++ public/pages/pending_orders.php
@@ -129,7 +129,7 @@ function getSortIcon($column, $currentColumn, $currentDirection) {
         
         /* Account for scrollbar width in header */
         .summary-table thead {
-            width: calc(100% - 17px);
+            width: 100%;
         }
         
         /* Cell styling for proper alignment and text overflow */
