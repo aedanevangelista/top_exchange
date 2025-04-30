8a720d34ba0414553081511dee7e424638ee00b2
Aedan Evangelista <103922745+aedanevangelista@users.noreply.github.com>
2025-04-30 21:01:41 +0800

    Update deliverable_orders.php

--- admin/public/pages/deliverable_orders.php
+++ admin/public/pages/deliverable_orders.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-include "../../admin/backend/db_connection.php";
-include "../../admin/backend/check_role.php";
+include "../../../backend/db_connection.php";
+include "../../../backend/check_role.php";
 checkRole('Orders'); // Ensure user has access to Orders
 
 // Get current date for auto-transit
